2ae5ee16a14f50af4ff8f217fdf12953a7ac9287
Rob Hardy <ra_hardy@hotmail.com>
2020-09-28 18:51:47 +0100

    added source map to bundle build

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,19 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import browsersync from 'rollup-plugin-browsersync';
+import sourcemaps from 'rollup-plugin-sourcemaps';
 
 export default {
     input: 'src/app.js',
     output: {
         file: 'dist/appbundle.js',
-        format: 'es'
+        format: 'es',
+        sourcemap: true,
   },
   plugins: [
     nodeResolve(),
     browsersync({
       server: '.'
-    })
+    }),
+    sourcemaps(),
   ]
 };
