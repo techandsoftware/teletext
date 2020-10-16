7aa281f4ea6ee1b4d3df53671d176d7f7c6e6d3d
Rob Hardy <ra_hardy@hotmail.com>
2020-10-16 15:41:59 +0100

    tidying code in view

--- src/VectorView.js
+++ src/VectorView.js
@@ -40,11 +40,11 @@ export class View {
         this._svg = SVG().addTo('#teletextscreen')
             .viewbox(`0 0 ${WIDTH_PX - 1} ${HEIGHT_PX - 1}`)
             .size(WIDTH_PX * SCREEN_SCALE, HEIGHT_PX * SCREEN_SCALE * ASPECT_RATIO_VERTICAL_SCALE[DEFAULT_ASPECT_RATIO])
-            .attr('preserveAspectRatio', 'none');
-
+            .attr('preserveAspectRatio', 'none')
+        
+        this._svg.style(getStyle());
         this.d = this._svg.group().attr('class', 'conceal_concealed flash_flashing');
 
-        this._createStyle();
         this._createRowBackgrounds();
         this._createCells();
         this._createBoxModeClip();
@@ -57,68 +57,6 @@ export class View {
         this._boxMode = false;
         this._mixMode = false;
         console.debug('VectorView constructed');
-    }
-
-    _createStyle() {
-        const style =
-`@font-face {
-    font-family: 'Unscii';
-    src: url('fonts/unscii-16.woff') format('woff'), 
-        url('fonts/unscii-16.ttf') format('truetype'),
-        url('fonts/unscii-16.otf') format('opentype');
-    unicode-range: U+20, U+2022, U+2500, U+2502, U+250C, U+2510, U+2514, U+2518, U+251C, U+251D, U+2524, U+2525, U+252C, U+252F, U+2534, U+2537, U+253C, U+253F, U+2588, U+258C, U+2590, U+2592, U+25CB, U+25CF, U+25E2-25E5, U+2B60-2B63, U+E0C0-E0FF, U+1FB00-1FB70, U+1FB75, U+1FBA0-1FBA7;
-    -webkit-font-smoothing: none;
-    font-smooth: never;
-}
-@keyframes blink {
-    66% {
-        visibility: hidden;
-    }
-    to {
-        visibility: hidden;
-    }
-}
-@keyframes fancyblink {
-    0% {
-        filter: none;
-        opacity: 0.8;
-    }
-    33% {
-        filter: none;
-        opacity: 1;
-    }
-    66% {
-        filter: none;
-        opacity: 1;
-    }
-    to {
-        filter: blur(1px);
-        opacity: 0;
-    }
-}
-.mosaic {
-    font-family: 'Unscii';
-    font-size: 10.1px; /* FUDGE bigger than 10px to close tiny gaps vertically */
-}
-.mosaic_separated {
-    font-family: 'Unscii';
-    font-size: 10px;
-}
-.flash_flashing .flash {
-    /* animation: blink 2s steps(3, start) infinite; */
-    animation: fancyblink 2s linear infinite;
-}
-.conceal_concealed  .conceal {
-    visibility: hidden;
-}
-svg #background {
-    transition-property: opacity;
-    transition-duration: 0.25s;
-}
-svg {
-    background-color: transparent;
-}`;
-        this._svg.style(style);
     }
 
     _update() {
@@ -141,14 +79,14 @@ svg {
                 const isMosaicByte = cell.isMosaicByte();
                 const fill = Attributes.fillColourFromColourAttrib(cell.fgColour);
                 const bg = Attributes.fillColourFromColourAttrib(cell.bgColour);
-                const attr = View._getCellAttr(cell.type, isMosaicByte);
+                const attr = getCellAttr(cell.type, isMosaicByte);
 
                 cellView.plain(cell.char).attr(attr).fill(fill);
                 if (cell.size == CellSize.DOUBLE_HEIGHT) {
                     const yTranslate = (2 * ((CELL_HEIGHT * rowIndex) + TEXT_Y_OFFSET)) - ((CELL_HEIGHT * rowIndex) + (2 * TEXT_Y_OFFSET))
                     cellView.attr('transform', `translate(0 -${yTranslate}) scale(1 2)`);
                 }
-                View._setCellClasses(cellView, cell.type, cell.flashing, cell.concealed, isMosaicByte);
+                setCellClasses(cellView, cell.type, cell.flashing, cell.concealed, isMosaicByte);
 
                 if (cell.boxed) {
                     if (previousBoxed) this._extendBox();
@@ -215,44 +153,6 @@ svg {
             this._boxMode = false;
             console.log('box deactivated');
         }
-    }
-
-    static _setCellClasses(cellView, cellType, flashing, concealed, isMosaic) {
-        if (cellType == CellType.MOSAIC_CONTIGUOUS && isMosaic) cellView.addClass('mosaic');
-        else if (cellType == CellType.MOSAIC_SEPARATED && isMosaic) cellView.addClass('mosaic_separated');
-
-        if (flashing) cellView.addClass('flash');
-        if (concealed) cellView.addClass('conceal');
-    }
-
-    static _getCellAttr(cellType, isMosaicChar) {
-        if (cellType == CellType.MOSAIC_CONTIGUOUS && isMosaicChar) {
-            return {
-                dx: MOSAIC_METRIC.contiguous.DX,
-                textLength: MOSAIC_METRIC.contiguous.textLength,
-                lengthAdjust: 'spacingAndGlyphs',
-                'text-anchor': 'start',
-                transform: null,
-                class: null,
-            };
-        } else if (cellType == CellType.MOSAIC_SEPARATED && isMosaicChar) {
-            return {
-                dx: MOSAIC_METRIC.separated.DX,
-                textLength: MOSAIC_METRIC.separated.textLength,
-                lengthAdjust: 'spacingAndGlyphs',
-                'text-anchor': 'start',
-                transform: null,
-                class: null,
-            };
-        } 
-        return {
-            dx: null,
-            textLength: null,
-            lengthAdjust: null,
-            'text-anchor': null,
-            transform: null,
-            class: null,
-        };
     }
 
     _drawGrid() {
@@ -383,16 +283,106 @@ svg {
             this.boxLayer.add(box);
         });
     }
+}
 
-    setTestPage() {
-        this.gridrows.forEach(row => {
-            row.forEach(cell => {
-                cell.plain(getRandomLetter())
-            });
-        });
-    }
+function getCellAttr(cellType, isMosaicChar) {
+    if (cellType == CellType.MOSAIC_CONTIGUOUS && isMosaicChar) {
+        return {
+            dx: MOSAIC_METRIC.contiguous.DX,
+            textLength: MOSAIC_METRIC.contiguous.textLength,
+            lengthAdjust: 'spacingAndGlyphs',
+            'text-anchor': 'start',
+            transform: null,
+            class: null,
+        };
+    } else if (cellType == CellType.MOSAIC_SEPARATED && isMosaicChar) {
+        return {
+            dx: MOSAIC_METRIC.separated.DX,
+            textLength: MOSAIC_METRIC.separated.textLength,
+            lengthAdjust: 'spacingAndGlyphs',
+            'text-anchor': 'start',
+            transform: null,
+            class: null,
+        };
+    } 
+    return {
+        dx: null,
+        textLength: null,
+        lengthAdjust: null,
+        'text-anchor': null,
+        transform: null,
+        class: null,
+    };
 }
 
 function getRandomLetter() {
     return String.fromCharCode(32 + Math.random() * 95); // returns letter in ASCII range
+}
+
+function setCellClasses(cellView, cellType, flashing, concealed, isMosaic) {
+    if (cellType == CellType.MOSAIC_CONTIGUOUS && isMosaic) cellView.addClass('mosaic');
+    else if (cellType == CellType.MOSAIC_SEPARATED && isMosaic) cellView.addClass('mosaic_separated');
+
+    if (flashing) cellView.addClass('flash');
+    if (concealed) cellView.addClass('conceal');
+}
+
+function getStyle() {
+    return `@font-face {
+font-family: 'Unscii';
+src: url('fonts/unscii-16.woff') format('woff'), 
+url('fonts/unscii-16.ttf') format('truetype'),
+url('fonts/unscii-16.otf') format('opentype');
+unicode-range: U+20, U+2022, U+2500, U+2502, U+250C, U+2510, U+2514, U+2518, U+251C, U+251D, U+2524, U+2525, U+252C, U+252F, U+2534, U+2537, U+253C, U+253F, U+2588, U+258C, U+2590, U+2592, U+25CB, U+25CF, U+25E2-25E5, U+2B60-2B63, U+E0C0-E0FF, U+1FB00-1FB70, U+1FB75, U+1FBA0-1FBA7;
+-webkit-font-smoothing: none;
+font-smooth: never;
+}
+@keyframes blink {
+66% {
+visibility: hidden;
+}
+to {
+visibility: hidden;
+}
+}
+@keyframes fancyblink {
+0% {
+filter: none;
+opacity: 0.8;
+}
+33% {
+filter: none;
+opacity: 1;
+}
+66% {
+filter: none;
+opacity: 1;
+}
+to {
+filter: blur(1px);
+opacity: 0;
+}
+}
+.mosaic {
+font-family: 'Unscii';
+font-size: 10.1px; /* FUDGE bigger than 10px to close tiny gaps vertically */
+}
+.mosaic_separated {
+font-family: 'Unscii';
+font-size: 10px;
+}
+.flash_flashing .flash {
+/* animation: blink 2s steps(3, start) infinite; */
+animation: fancyblink 2s linear infinite;
+}
+.conceal_concealed  .conceal {
+visibility: hidden;
+}
+svg #background {
+transition-property: opacity;
+transition-duration: 0.25s;
+}
+svg {
+background-color: transparent;
+}`;
 }
