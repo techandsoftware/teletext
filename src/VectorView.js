a1970f6f99335a4a7638e14827e4ed16d7306bc9
Rob Hardy <ra_hardy@hotmail.com>
2020-10-16 14:39:37 +0100

    svg styling is now embedded in the svg instead of the enclosing html

--- src/VectorView.js
+++ src/VectorView.js
@@ -44,6 +44,7 @@ export class View {
 
         this.d = this._svg.group().attr('class', 'conceal_concealed flash_flashing');
 
+        this._createStyle();
         this._createRowBackgrounds();
         this._createCells();
         this._createBoxModeClip();
@@ -56,6 +57,68 @@ export class View {
         this._boxMode = false;
         this._mixMode = false;
         console.debug('VectorView constructed');
+    }
+
+    _createStyle() {
+        const style =
+`@font-face {
+    font-family: 'Unscii';
+    src: url('fonts/unscii-16.woff') format('woff'), 
+        url('fonts/unscii-16.ttf') format('truetype'),
+        url('fonts/unscii-16.otf') format('opentype');
+    unicode-range: U+20, U+2022, U+2500, U+2502, U+250C, U+2510, U+2514, U+2518, U+251C, U+251D, U+2524, U+2525, U+252C, U+252F, U+2534, U+2537, U+253C, U+253F, U+2588, U+258C, U+2590, U+2592, U+25CB, U+25CF, U+25E2-25E5, U+2B60-2B63, U+E0C0-E0FF, U+1FB00-1FB70, U+1FB75, U+1FBA0-1FBA7;
+    -webkit-font-smoothing: none;
+    font-smooth: never;
+}
+@keyframes blink {
+    66% {
+        visibility: hidden;
+    }
+    to {
+        visibility: hidden;
+    }
+}
+@keyframes fancyblink {
+    0% {
+        filter: none;
+        opacity: 0.8;
+    }
+    33% {
+        filter: none;
+        opacity: 1;
+    }
+    66% {
+        filter: none;
+        opacity: 1;
+    }
+    to {
+        filter: blur(1px);
+        opacity: 0;
+    }
+}
+.mosaic {
+    font-family: 'Unscii';
+    font-size: 10.1px; /* FUDGE bigger than 10px to close tiny gaps vertically */
+}
+.mosaic_separated {
+    font-family: 'Unscii';
+    font-size: 10px;
+}
+.flash_flashing .flash {
+    /* animation: blink 2s steps(3, start) infinite; */
+    animation: fancyblink 2s linear infinite;
+}
+.conceal_concealed  .conceal {
+    visibility: hidden;
+}
+svg #background {
+    transition-property: opacity;
+    transition-duration: 0.25s;
+}
+svg {
+    background-color: transparent;
+}`;
+        this._svg.style(style);
     }
 
     _update() {
