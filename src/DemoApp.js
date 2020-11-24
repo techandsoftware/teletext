2033ee002573c32f34dc318a8255d00732d425da
Rob Hardy <ra_hardy@hotmail.com>
2020-11-24 17:31:57 +0000

    use npm-published version of the smooth mosaic plugin

--- src/DemoApp.js
+++ src/DemoApp.js
@@ -1,5 +1,5 @@
 import { teletextjs, Attributes, Colour, Level } from '../dist/teletextjs.js';
-import { SmoothMosaicPlugin } from 'teletext-plugin-smooth-mosaic';
+import { SmoothMosaicPlugin } from '@techandsoftware/teletext-plugin-smooth-mosaic';
 
 const ASPECT_RATIOS = [1.2, 1.22, 1.33, 'natural'];
 const FONTS = ['sans-serif', 'Bedstead', 'native', 'serif', 'Unscii', 'Ubuntu'];
