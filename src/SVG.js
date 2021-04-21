4143c9718fc480e5ea3b12cbba04e0251bfb09e5
Rob Hardy <ra_hardy@hotmail.com>
2021-04-21 12:33:37 +0100

    improved error handling on addTo when the selector doesn't match

--- src/SVG.js
+++ src/SVG.js
@@ -110,7 +110,12 @@ export class SVG extends Element {
     }
 
     addTo(selector) {
-        _doc.querySelector(selector).appendChild(this._e);
+        const node = _doc.querySelector(selector);
+        if (node) {
+            node.appendChild(this._e);
+        } else {
+            throw new Error('@techandsoftware/teletext: E117: addTo failed to match provided selector')
+        }
         return this;
     }
 
