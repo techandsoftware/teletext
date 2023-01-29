c656b03b6946bc8195f8305474e9442bd0e939cd
Rob H <techandsoftware_commits@outlook.com>
2023-01-29 18:42:02 +0000

    rewrote decodeBase64URLEncoded to remove dependency on uint1array

--- src/Utils.js
+++ src/Utils.js
@@ -1,7 +1,7 @@
-// SPDX-FileCopyrightText: © 2021 Tech and Software Ltd.
+// SPDX-FileCopyrightText: © 2023 Tech and Software Ltd.
 // SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-uk.ltd.TechAndSoftware-1.0
 
-import Uint1Array from 'uint1array';
+// import Uint1Array from 'uint1array';
 
 // Arabic chars in initial, medial or final form are cursive
 const CURSIVE_CHARS = "ﻰﺋﺊﭼﭽﭘﭙﮔﻎﻼﻬﻪﻊﺔﺒﺘﺎﺑﺗﺛﺟﺣﺧﺳﺷﺻﺿﻃﻇﻋﻏﺜﺠﺤﺨـﻓﻗﻛﻟﻣﻧﻫﻰﻳﻴﻌﻐﻔﻘﻠﻤﻨ";
@@ -10,26 +10,30 @@ export class Utils {
 
     // "base64url" encoding defined here https://tools.ietf.org/html/rfc4648
     // the packed data format is from https://github.com/rawles/edit.tf
-    // returns a Uint1Array
     static decodeBase64URLEncoded_(input, atob) {
-        // adjust the input before passing to atob
+        // convert URL-encoded to real base 64
         input = input.replace(/-/g, '+').replace(/_/g, '/');
         const pad = input.length % 4;
         if (pad) {
             if (pad === 1) throw new Error('Utils.decodeBase64URLEncoded E16: Input base64url string is the wrong length to determine padding');
-            input += new Array(5-pad).join('=');
+            input += new Array(5 - pad).join('=');
         }
-        const packed = atob(input);
+        const data = atob(input); // MSB first
 
-        // FUDGE as Unit8Array.set stores LSB first but we want MSB first in the bit array, getMsbCode reverses the bits
-        const msbCodes = [...packed].map(c => getMsbCode(c));
-        
-        const buffer = new ArrayBuffer(msbCodes.length);
-        const bytes = new Uint8Array(buffer);
-        bytes.set(msbCodes);
-        const bits = new Uint1Array(buffer);
+        // convert octets to rows of 7 bit chars
+        const rows = [];
+        let row = [];
+        for (const val of dataTo7Bits(data)) {
+            row.push(String.fromCharCode(val));
+            if (row.length == 40) {
+                rows.push(row.join(''));
+                row = [];
+            }
+        }
+        if (row.length < 40)
+            rows.push(row.join(''));
 
-        return getUnpackedData(bits);
+        return rows;
     }
 
     // Output Line format from .tti file format https://zxnet.co.uk/teletext/documents/ttiformat.pdf
@@ -59,7 +63,6 @@ export class Utils {
         return decoded;
     }
 
-
     static getRowsFromOutputLines_(lines) {
         const rows = [];
         const regEx = /^OL,(\d{1,2}),(.*)/
@@ -79,52 +82,32 @@ export class Utils {
     }
 }
 
-const msbCodes = {};
-
-// get charCode for a char with the bit significance reversed
-function getMsbCode(char) {
-    if (char in msbCodes) return msbCodes[char];
-    
-    const msbBits = [...char.charCodeAt(0).toString(2).padStart(8, '0')].reverse();
-    msbCodes[char] = Number.parseInt(msbBits.join(''), 2); 
-    return msbCodes[char];
+function intToBits(n) {
+    let bits = [];
+    for (let b = 7; b >= 0; b--) {
+        bits.push(n & (1 << b) ? 1 : 0);
+    }
+    return bits;
 }
 
-// or is this better?
-// function getMsbCode(char) {
-//     if (char in msbCodes) return msbCodes[char];
-
-//     const code = char.charCodeAt(0);
-//     msbCodes[char] =
-//               ((code & 0b10000000) >> 7)
-//             + ((code & 0b01000000) >> 5)
-//             + ((code & 0b00100000) >> 3)
-//             + ((code & 0b00010000) >> 1)
-//             + ((code & 0b00001000) << 1)
-//             + ((code & 0b00000100) << 3)
-//             + ((code & 0b00000010) << 5)
-//             + ((code & 0b00000001) << 7);
-//     return msbCodes[char];
-// }
-
-
-function getUnpackedData(bitArray) {
-    // const firstBitIndex = (280 * row) + (7 * col);
-    const page = [];
-
-    for (let r = 0; r < 25; r++) {
-        const rowChars = [];
-        for (let c = 0; c < 40; c++) {
-            let bitSignificance = 6;
-            let charCode = 0;
-            const firstBitIndex = (280 * r) + (7 * c);
-            for (let bit = firstBitIndex; bit < firstBitIndex + 7; bit++) {
-                charCode += bitArray[bit] * Math.pow(2, bitSignificance);
-                bitSignificance--;
+// unpacks MSB-first 8-bit bytes to 7-bit bytes
+//   01000000 10000001 00000001 ...
+// = 0100000 0100000 0100000 01...
+function* dataTo7Bits(data) {
+    let bShift = 6;
+    let val = 0;
+    for (const d of data) {
+        const bits = intToBits(d.charCodeAt(0));
+        for (const b of bits) {
+            val |= b << bShift;
+            bShift--;
+            if (bShift < 0) {
+                yield val;
+                bShift = 6;
+                val = 0;
             }
-            rowChars.push(String.fromCharCode(charCode));
         }
-        page.push(rowChars.join(''));
     }
-    return page;
+
+    if (bShift < 6) yield val;
 }
