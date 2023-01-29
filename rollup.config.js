24f6c0adb43bcdf49058c2b1c46f740b6cd793d6
Rob H <techandsoftware_commits@outlook.com>
2023-01-29 22:33:31 +0000

    removed spdx copyright for uint1array in dist packages as it's no longer needed

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,11 @@
-// SPDX-FileCopyrightText: © 2021 Tech and Software Ltd.
+// SPDX-FileCopyrightText: © 2023 Tech and Software Ltd.
 // SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-uk.ltd.TechAndSoftware-1.0
 
 import resolve from '@rollup/plugin-node-resolve';
 import browsersync from 'rollup-plugin-browsersync';
 import json from '@rollup/plugin-json';
 
-const OUTPUT_BANNER = `// SPDX${''}-FileCopyrightText: (c) 2021 Tech and Software Ltd.
-// SPDX${''}-FileCopyrightText: (c) 2017 dosaygo
+const OUTPUT_BANNER = `// SPDX${''}-FileCopyrightText: (c) 2023 Tech and Software Ltd.
 // SPDX${''}-License-Identifier: AGPL-3.0-only OR LicenseRef-uk.ltd.TechAndSoftware-1.0
 // LicenseRef-uk.ltd.TechAndSoftware-1.0 refers to https://tech-and-software.ltd.uk/LICENSES/LicenseRef-uk.ltd.TechAndSoftware-1.0.txt`;
 
--- rollup.prod.config.js
+++ rollup.prod.config.js
@@ -6,8 +6,7 @@ import terser from "@rollup/plugin-terser";
 import json from '@rollup/plugin-json';
 
 // preamble in the minified source
-const OUTPUT_BANNER = `// SPDX${''}-FileCopyrightText: (c) 2021 Tech and Software Ltd.
-// SPDX${''}-FileCopyrightText: (c) 2017 dosaygo
+const OUTPUT_BANNER = `// SPDX${''}-FileCopyrightText: (c) 2023 Tech and Software Ltd.
 // SPDX${''}-License-Identifier: AGPL-3.0-only OR LicenseRef-uk.ltd.TechAndSoftware-1.0
 // LicenseRef-uk.ltd.TechAndSoftware-1.0 refers to https://tech-and-software.ltd.uk/LICENSES/LicenseRef-uk.ltd.TechAndSoftware-1.0.txt`;
 
