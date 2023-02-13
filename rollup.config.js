763d7f3c2361f9427078e0172e69fb31318400cb
Rob H <techandsoftware_commits@outlook.com>
2023-02-13 00:08:16 +0000

    rollup - banner not needed on unminimised dist

--- rollup.config.js
+++ rollup.config.js
@@ -5,11 +5,6 @@ import resolve from '@rollup/plugin-node-resolve';
 import browsersync from 'rollup-plugin-browsersync';
 import json from '@rollup/plugin-json';
 
-const OUTPUT_BANNER = `// SPDX${''}-FileCopyrightText: (c) 2023 Tech and Software Ltd.
-// SPDX${''}-License-Identifier: AGPL-3.0-only OR LicenseRef-uk.ltd.TechAndSoftware-1.0
-// LicenseRef-uk.ltd.TechAndSoftware-1.0 refers to https://tech-and-software.ltd.uk/LICENSES/LicenseRef-uk.ltd.TechAndSoftware-1.0.txt`;
-
-
 export default {
   input: {
     teletext: 'src/app.js',
@@ -20,7 +15,6 @@ export default {
     format: 'es',
     sourcemap: true,
     sourcemapExcludeSources: true,
-    banner: OUTPUT_BANNER
   },
   plugins: [
     resolve(),
