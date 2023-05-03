6e80e3f58d94a0240a1516f3e5afbff18eaaf113
Rob H <techandsoftware_commits@outlook.com>
2023-05-03 22:52:44 +0100

    added TODO to Cell

--- src/Cell.js
+++ src/Cell.js
@@ -69,7 +69,7 @@ export class Cell {
             this._char = getCharWithEncoding(this._byte, encoding);
             if (this._diacriticCode > 0) this._char += encodings["g2_latin"][String.fromCharCode(this._diacriticCode + 0x40)];
             this._isCursive = false;
-            if (encoding == 'g0_arabic' || encoding == 'g2_arabic') this._isCursive = Utils.isCursive_(this._char);
+            if (encoding == 'g0_arabic' || encoding == 'g2_arabic') this._isCursive = Utils.isCursive_(this._char); // TODO fix
         } else if (this._type == CellType.MOSAIC_CONTIGUOUS_)
             this._char = getCharWithEncoding(this._byte, 'g1_block_mosaic_to_unicode__legacy_computing');
         else if (this._type == CellType.MOSAIC_SEPARATED_)
