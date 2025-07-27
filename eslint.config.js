12de2646d85cae655e50f574555b2fcd5d871c54
Rob H <techandsoftware_commits@outlook.com>
2025-07-27 16:33:48 +0100

    exclude coverage from lint

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { ignores: ["dist/**"] },
+  { ignores: ["dist/**", "coverage/**"] },
   {
     files: ["lib/*.js"],
     plugins: { js },
