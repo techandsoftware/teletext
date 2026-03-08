97b27e01ef7cc29ce577e9529b5da68c5af56b2c
Rob H <techandsoftware_commits@outlook.com>
2026-03-08 14:44:01 +0000

    refactored vite build and remove need for external terser for minification

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: © 2025 Rob Hardy
+// SPDX-FileCopyrightText: © 2026 Rob Hardy
 // SPDX-License-Identifier: AGPL-3.0-only
 
 import { dirname, resolve } from 'node:path';
@@ -7,27 +7,55 @@ import { defineConfig } from 'vite';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// issue - vite doesn't minify es libs https://github.com/vitejs/vite/issues/6555 so we minify after vite in the build script
+const BANNER = `/*! @techandsoftware/teletext
+    https://www.npmjs.com/package/@techandsoftware/teletext
+    SPDX${''}-FileCopyrightText: (c) ${new Date().getUTCFullYear()} Rob Hardy
+    SPDX${''}-License-Identifier: AGPL-3.0-only
+ */`;
 
-const BANNER = `/*! SPDX${''}-FileCopyrightText: (c) ${new Date().getUTCFullYear()} Rob Hardy
-    SPDX${''}-License-Identifier: AGPL-3.0-only */`;
 
-export default defineConfig({
-  publicDir: 'demo',
-  build: {
-    sourcemap: true,
-    copyPublicDir: false,
-    emptyOutDir: false,
-    lib: {
-      entry: resolve(__dirname, 'lib/app.js'),
-      name: '@techandsoftware/teletext',
-      fileName: 'teletext',
-      formats: ['es']
+export default defineConfig(({ mode }) => {
+
+  const isMinified = mode === "minified";
+
+  // used for minified buld
+  const terserConfig = {
+    ecma: 2016,
+    toplevel: true,
+    compress: {
+      drop_console: true,
+      passes: 2,
+      pure_getters: true,
+      unsafe: true,
+      unsafe_symbols: true,
+      unsafe_arrows: true
     },
-    rollupOptions: {
-      output: {
-        banner: BANNER,
+    mangle: {
+      properties: {
+        regex: "^_.+|.+_$"
       }
+    },
+    format: {
+      comments: /^!|@license|@preserve/i,
+      preserve_annotations: true,
+      preamble: BANNER
+    }
+  };
+
+  return {
+    publicDir: "demo",
+    build: {
+      copyPublicDir: false,
+      emptyOutDir: false,
+      sourcemap: !isMinified,
+      lib: {
+        entry: resolve(__dirname, "lib/app.js"),
+        name: "@techandsoftware/teletext",
+        fileName: isMinified ? "teletext.min" : "teletext",
+        formats: ["es"]
+      },
+      minify: isMinified ? "terser" : false,
+      terserOptions: isMinified ? terserConfig : undefined
     }
   }
 });
